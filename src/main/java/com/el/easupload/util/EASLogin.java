859c92ecae45e7927d9856a9991b24fc30683b84
mcnalwan <alan_007@foxmail.com>
2021-08-04 18:28:52 +0800

    V1.1

--- src/main/java/com/el/easupload/util/EASLogin.java
+++ src/main/java/com/el/easupload/util/EASLogin.java
@@ -1,39 +1,52 @@
 package com.el.easupload.util;
 
-import com.el.easupload.ws.EASLoginProxy;
-import com.el.easupload.ws.EASLoginProxyServiceLocator;
-import com.el.easupload.ws.WSContext;
-import javax.xml.rpc.ServiceException;
-import java.net.MalformedURLException;
+import com.el.easupload.ws.*;
 
-import static com.el.easupload.util.Resource.URL_LOGIN;
+import javax.xml.rpc.Service;
+import javax.xml.rpc.ServiceException;
+import java.awt.event.ActionEvent;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 
 public class EASLogin {
-    public WSContext doEASLogin(){
-        java.net.URL endpoint =null;
+    public WSContext doEASLogin() {
+        java.net.URL endpoint = null;
 
-            try {
-
-                endpoint=new java.net.URL(URL_LOGIN);
-
-
-        EASLoginProxy proxy=null;
-
-            proxy =new EASLoginProxyServiceLocator().getEASLogin(endpoint);
-
-            return  proxy.login(Resource.USERNAME,Resource.PASSWORD,Resource.SLNNAME,Resource.DBCODE,
-                    Resource.LANGUAGE,Resource.DBTYPE);
+        try {
+            endpoint = new java.net.URL(Resource.URL_LOGIN);
+            EASLoginProxy proxy = null;
+            proxy = new EASLoginProxyServiceLocator().getEASLogin(endpoint);
+            return proxy.login(Resource.USERNAME, Resource.PASSWORD, Resource.SLNNAME, Resource.DBCODE,
+                    Resource.LANGUAGE, Resource.DBTYPE);
         } catch (ServiceException e) {
             e.printStackTrace();
 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-
-            }finally {
-                return null;
-            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } finally {
+            return null;
+        }
     }
 
+    public void impVoucher() {
+        WSWSVoucher[] voucher = null;
 
+        try {
+            java.net.URL endpoint = new java.net.URL(Resource.URL_GL);
+            int verify = 0;
+            int cashflow = 0;
+            WSGLWebServiceFacadeSrvProxyServiceLocator locator = new WSGLWebServiceFacadeSrvProxyServiceLocator();
+            locator.getWSGLWebServiceFacade(endpoint).importVoucher(voucher, 0, verify, cashflow);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        } catch (WSInvokeException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/test/java/com/el/easupload/Test.java
+++ src/test/java/com/el/easupload/Test.java
@@ -16,6 +16,7 @@ public class Test {
         System.out.println(WSContext.getTypeDesc());
 
 
+
     }
 
 }
